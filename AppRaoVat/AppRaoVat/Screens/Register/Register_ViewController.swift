4a4ace1b0da779a479d827d570415f2c0864a182
phuln <phuln@ominext.com>
2022-08-16 10:12:23 +0700

    upload image to server from xcode

--- AppRaoVat/AppRaoVat/Screens/Register/Register_ViewController.swift
+++ AppRaoVat/AppRaoVat/Screens/Register/Register_ViewController.swift
@@ -31,7 +31,31 @@ class Register_ViewController: UIViewController, UINavigationControllerDelegate,
     }
     
     @IBAction func uploadImageToServer(_ sender: Any) {
+        let url = URL(string: "http://10.50.80.101:3000/uploadFile")
+        let boundary = UUID().uuidString
+        let session = URLSession.shared
         
+        var urlRequest = URLRequest(url: url!)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        var data = Data()
+        data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
+        data.append("Content-Disposition: form-data; name=\"avatar\"; filename=\"avatar.png\"\r\n".data(using: .utf8)!)
+        data.append("Content-Type: image/png\r\n\r\n".data(using: .utf8)!)
+        data.append((imgAvatar.image?.pngData())!)
+        data.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
+        
+        session.uploadTask(with: urlRequest, from: data, completionHandler: {
+            responseData, response, error in
+            if (error == nil) {
+                let jsonData = try? JSONSerialization.jsonObject(with: responseData!, options: .allowFragments)
+                if let json = jsonData as? [String:Any] {
+                    print(json)
+                }
+            }
+            }
+        ).resume()
     }
     
 }
